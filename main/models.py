5db577b6d4bf18c1955897b553b28c062de74d3a
BrMaro <70138725+BrMaro@users.noreply.github.com>
2024-05-23 17:51:19 +0300

    initial model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.IntegerField()
+    image = models.ImageField(upload_to='product_images/')
+    category = models.ForeignKey("Category",on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through="OrderProduct")
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status_choices = [
+        ('pending', 'Pending'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+    ]
+    status = models.CharField(max_length=20, choices=status_choices)
+    order_date = models.DateTimeField(auto_now_add=True)
+    delivery_date = models.DateTimeField(null=True, blank=True)
+    shipping_address = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
